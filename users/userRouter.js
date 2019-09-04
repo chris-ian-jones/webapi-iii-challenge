80acc7fdfedc412eec151276282628d2c2112597
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-04 21:35:05 +0100

    WIP: setup GET endpoint for '/' in userRouter.js

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,17 @@ const express = require('express')
 
 const router = express.Router();
 
+const db = require('./userDb')
+
+function validateUserId(req, res, next) {
+  if (!req.id) {
+    res.status(400).json({
+      message: 'invalid user id'
+    })
+  }
+  next();
+}
+
 router.post('/', (req, res) => {
 
 });
@@ -11,7 +22,19 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  res.send('Api for /api/users is working...')
+  db.get()
+    .then(users => {
+      res.status(200).json({
+        users
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        error: 'The posts could not be retirbved from the database'
+      })
+
+    })
 });
 
 router.get('/:id', (req, res) => {
